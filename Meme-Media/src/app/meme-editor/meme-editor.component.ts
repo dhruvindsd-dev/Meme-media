e136fd8f96cc738b3922fabd4c49afa9f696d7b1
dhruvindsd-dev <dhruvindsd@gmail.com>
2020-10-11 13:46:32 +0530

    - added max-width of the line feature in meme-editor

--- Meme-Media/src/app/meme-editor/meme-editor.component.ts
+++ Meme-Media/src/app/meme-editor/meme-editor.component.ts
@@ -14,14 +14,16 @@ export class MemeEditorComponent implements OnInit {
     text: string;
     fontSize: number;
     max_words_per_line: number;
-    x_pos;
-    y_pos;
+    x_pos: number;
+    y_pos: number;
+    max_width: number;
   } = {
     text: 'Enter Top text',
     fontSize: 20,
     max_words_per_line: 2,
     x_pos: 10,
     y_pos: 10,
+    max_width: 300,
   };
   bottomObj: { text: string; fontSize: number } = {
     text: 'Enter the Botton Text',
@@ -86,39 +88,59 @@ export class MemeEditorComponent implements OnInit {
     }
   }
 
+  // nxtLineShifter() {
+  //   this.svgtxt.nativeElement.innerHTML = '';
+
+  //   const wordsList = this.topObj.text.trim().split(' ');
+
+  //   const chunked_arr = [];
+  //   let index = 0;
+  //   while (index < wordsList.length) {
+  //     chunked_arr.push(
+  //       wordsList.slice(index, index + this.topObj.max_words_per_line)
+  //     );
+  //     index += this.topObj.max_words_per_line;
+  //   }
+  //   console.log(chunked_arr);
+
+  //   let before_ele = null;
+  //   for (let i of chunked_arr) {
+  //     const spanEle = this.createTspan(i.join(' ').trim(), before_ele);
+  //     this.svgtxt.nativeElement.appendChild(spanEle);
+  //     before_ele = spanEle;
+  //   }
+  // }
   nxtLineShifter() {
     this.svgtxt.nativeElement.innerHTML = '';
-
     const wordsList = this.topObj.text.trim().split(' ');
-
-    const chunked_arr = [];
+    // some way to add text into hte dom and check if its length is greater then our max width, if it is remove a word and add to a new element
     let index = 0;
+    const spanEle = this.createTspan(' ');
+    this.svgtxt.nativeElement.appendChild(spanEle);
     while (index < wordsList.length) {
-      chunked_arr.push(
-        wordsList.slice(index, index + this.topObj.max_words_per_line)
-      );
-      index += this.topObj.max_words_per_line;
-    }
-    console.log(chunked_arr);
-
-    let before_ele = null;
-    for (let i of chunked_arr) {
-      const spanEle = this.createTspan(i.join(' ').trim(), before_ele);
-      this.svgtxt.nativeElement.appendChild(spanEle);
-      before_ele = spanEle;
-      console.log(i.join(' ').trim());
+      // ADD A WORD TO THE LAST EL .
+      // IF THEN LENGTH IS GREATER WIDTH THEN REMOVE TEH LAST EL AND CREATE A NEW TSPAN AND APPEDN WORD TO THAT
+      // ELSE : CONTUNUE
+      const lastEle = this.svgtxt.nativeElement.lastChild;
+      let lastEleText = lastEle.textContent;
+      lastEle.textContent = lastEleText + ' ' + wordsList[index];
+      const lastElementWidth = lastEle.getBBox().width;
+      if (lastElementWidth > this.topObj.max_width) {
+        lastEle.textContent = lastEleText;
+        const newTxtEl = this.createTspan(wordsList[index]);
+        this.svgtxt.nativeElement.appendChild(newTxtEl);
+      }
+      index++;
     }
   }
 
-  createTspan(txt, beforeEle) {
+  createTspan(txt, beforeEle = null) {
     var rect;
     if (!beforeEle) {
       rect = { width: 0 };
     } else {
       rect = beforeEle.getBBox();
     }
-    console.log(rect);
-
     const tempSpan = document.createElementNS(
       'http://www.w3.org/2000/svg',
       'tspan'
@@ -141,8 +163,12 @@ export class MemeEditorComponent implements OnInit {
     this.nxtLineShifter();
   }
   onTextChange(e) {
-    let a = e.target.value;
-    this.topObj.text = a;
+    this.topObj.text = e.target.value;
+    this.nxtLineShifter();
+  }
+  onWordsChange(e) {
+    this.topObj.max_width = parseInt(e.target.value);
+
     this.nxtLineShifter();
   }
 }
